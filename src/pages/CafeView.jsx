941d66007cd5acf27f9a7c42e18def8748752ec9
Inazumi Ikari <carzypop39@gmail.com>
2026-06-16 14:28:21 +0700

    fix(CafeView): fix BgModePanel undefined

--- src/pages/CafeView.jsx
+++ src/pages/CafeView.jsx
@@ -225,7 +225,7 @@ function CafeUpgradePanel({ state, onClose }) {
 }
 
 
-function BackgroundModePanel({ state, onClose }) {
+function BgModePanel({ state, onClose }) {
   
   const panelRef = useRef(null);
   const { bgMode } = state.cafe;
